2797a0b4669dcb709916b6fb38fb7641cb896151
zouhongzhao <zouhongzhao@126.com>
2018-06-02 09:42:17 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'benheart_magento2_zh_hans_cn',
+    'zouhongzhao_magento2_zh_hans_cn',
     __DIR__
 );
